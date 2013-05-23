40770f25e6228fb10471a4e987da5866930b7c5e
jaques.claudino@gmail.com <jaques.claudino@gmail.com@69850193-e186-7334-92bf-2fa3f42acedc>
2013-05-23 00:38:26 +0000

    first commit

---
+++ trunk/opendomotic-web/src/main/java/com/opendomotic/mb/DomoticMB.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.opendomotic.mb;
+
+import com.opendomotic.api.Device;
+import com.opendomotic.model.DeviceFactory;
+import com.opendomotic.model.DeviceProxy;
+import com.opendomotic.service.DeviceService;
+import com.opendomotic.service.TesteService;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Jaques
+ */
+@ManagedBean
+@SessionScoped
+public class DomoticMB implements Serializable {
+    
+    private static final Logger LOG = Logger.getLogger(DomoticMB.class.getName());
+
+    @EJB
+    private DeviceService deviceService;
+    
+    @EJB
+    private TesteService testeService;
+    
+    private List<Device> listDeviceButton;
+    
+    public DomoticMB() {
+        LOG.info("constructor");
+    }
+    
+    @PostConstruct()
+    public void init() {
+        LOG.info("postConstruct");
+        try {
+            createListDeviceButton();            
+            atualizar();            
+        } catch (Exception e) {
+            LOG.severe(e.getMessage());
+        }
+    }
+    
+    private void createListDeviceButton() {
+        listDeviceButton = new ArrayList<>();
+        listDeviceButton.add(deviceService.getDevice(DeviceFactory.DEVICE_NAME_LUZ_SALA));
+        listDeviceButton.add(deviceService.getDevice(DeviceFactory.DEVICE_NAME_LUZ_ESCRITORIO));
+        listDeviceButton.add(deviceService.getDevice(DeviceFactory.DEVICE_NAME_VENTILADOR));
+        listDeviceButton.add(deviceService.getDevice(DeviceFactory.DEVICE_NAME_CAFETEIRA));
+        listDeviceButton.add(deviceService.getDevice(DeviceFactory.DEVICE_NAME_TESTE_ETHERNET));
+    }
+    
+    public void atualizar() {
+        LOG.info("atualizar");
+        deviceService.updateDevices();
+    }
+    
+    public void toogleDevice(Device device) {
+        device.setValue(device.getValue() == 0 ? 1 : 0);
+    }
+    
+    public void toggleAll() {
+        boolean stateAll = !getStateAny();
+        
+        for (Device device : listDeviceButton) {
+            device.setValue(stateAll ? 1 : 0);
+        }
+    }
+    
+    private boolean getStateAny() {
+        for (Device device : listDeviceButton) {
+            if (device.getValue() == 1)
+                return true;
+        }
+        return false;
+    }
+    
+    public String getCaptionAll() {
+        return getStateAny() ? "Desligar tudo" : "Ligar tudo";
+    }
+
+    public List<DeviceProxy> getListDevice() {        
+        return deviceService.getListDevice();
+    }
+
+    public List<Device> getListDeviceButton() {
+        return listDeviceButton;
+    }
+
+    public String getTeste() {
+        return testeService.getList().toString();
+    }
+    
+    public void adicionarTeste() {
+        testeService.adicionar();
+    }
+    
+    public void excluirTeste() {
+        testeService.excluir();
+    }
+    
+}
---
+++ trunk/opendomotic-web/src/main/java/com/opendomotic/mb/LoginMB.java
@@ -0,0 +1,60 @@
+package com.opendomotic.mb;
+
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@SessionScoped
+public class LoginMB implements Serializable {
+
+    private String usuario;
+    private String senha;
+    private boolean autenticado = false;
+
+    public String login() throws ServletException {
+        //TO-DO: JAAS
+        autenticado = usuario.equals("user") && senha.equals("opendomotic");
+        
+        if (!autenticado) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Usuário ou senha incorreta!", null);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            return "login";
+        } else {
+            getSession().setAttribute("usuarioLogado", usuario);        
+            return "index";
+        }
+    }
+
+    public String logout() {
+        getSession().removeAttribute("usuarioLogado");
+        return "login";
+    }
+
+    private HttpSession getSession() {
+        return (HttpSession) FacesContext.getCurrentInstance()
+                    .getExternalContext().getSession(false);
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
