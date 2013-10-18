fa289760b8dbfea4db8923e9a3fcf6aea6607b29
jaques.claudino <jaques.claudino@69850193-e186-7334-92bf-2fa3f42acedc>
2013-10-18 21:41:24 +0000

    start 0.0.2

--- opendomotic-core/src/main/java/com/opendomotic/device/impl/PercentHttpDevice.java
+++ opendomotic-core/src/main/java/com/opendomotic/device/impl/PercentHttpDevice.java
@@ -6,35 +6,26 @@ import com.opendomotic.device.util.Percent;
  *
  * @author Jaques Claudino
  */
-public class PercentHttpDevice extends DoubleHttpDevice {
-    
-    public static final double DEFAULT_MIN = 0;
-    public static final double DEFAULT_MAX = 1023;
-    public static final boolean DEFAULT_CHECK_RANGE = true;
-    
-    private Double min = DEFAULT_MIN;
-    private Double max = DEFAULT_MAX;
+public class PercentHttpDevice extends DoubleHttpDevice {   
+
+    private final Percent percent = new Percent();
 
     @Override
     public Double getValue() throws Exception {
         Double value = super.getValue();
-        return Percent.getPercent(value, min, max, DEFAULT_CHECK_RANGE);
+        return percent.getPercent(value);
     }
     
-    public Double getMin() {
-        return min;
-    }
-
     public void setMin(Double min) {
-        this.min = min;
+        percent.setMin(min);
     }
-
-    public Double getMax() {
-        return max;
-    }
-
+    
     public void setMax(Double max) {
-        this.max = max;
+        percent.setMax(max);
+    }
+    
+    public void setAuto(boolean auto) {
+        percent.setAuto(auto);
     }
     
 }
