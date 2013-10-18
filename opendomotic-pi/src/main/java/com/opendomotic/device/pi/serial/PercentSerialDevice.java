cf956578a1155978c41fdc8f8062dc53110d9c4d
jaques.claudino <jaques.claudino@69850193-e186-7334-92bf-2fa3f42acedc>
2013-10-18 21:41:50 +0000

    start 0.0.2

--- opendomotic-pi/src/main/java/com/opendomotic/device/pi/serial/PercentSerialDevice.java
+++ opendomotic-pi/src/main/java/com/opendomotic/device/pi/serial/PercentSerialDevice.java
@@ -1,87 +1,31 @@
 package com.opendomotic.device.pi.serial;
 
 import com.opendomotic.device.util.Percent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Jaques Claudino
  */
-public class PercentSerialDevice extends SerialDevice {
-    
-    private static final Logger LOG = Logger.getLogger(PercentSerialDevice.class.getName());
-    
-    public static final int DEFAULT_MIN = 0;
-    public static final int DEFAULT_MAX = 1024;
-    public static final boolean DEFAULT_CHECK_RANGE = true;
-    public static final boolean DEFAULT_AUTO = false;
-    public static final boolean SHOW_LOG = false;
-    
-    private Integer min = null;
-    private Integer max = null;
-    private boolean checkRange = DEFAULT_CHECK_RANGE;
-    private boolean auto = DEFAULT_AUTO;
+public class PercentSerialDevice extends SerialDevice {  
+
+    private final Percent percent = new Percent();
     
     @Override
     public Integer getValue() throws Exception {
-        int value = (Integer) super.getValue();        
-        checkMinMax(value); 
-        return (int) Percent.getPercent(value, min, max, checkRange);
+        int value = (Integer) super.getValue(); 
+        return (int) percent.getPercent(value);
+    }   
+ 
+    public void setMin(Double min) {
+        percent.setMin(min);
     }
     
-    private void checkMinMax(int value) {
-        if (auto) {
-            if (min == null || value < min) {
-                min = value;
-                logMinMax();
-            }
-            if (max == null || value > max) {
-                max = value;
-                logMinMax();
-            }
-        } else if (min == null || max == null) {
-            min = DEFAULT_MIN;
-            max = DEFAULT_MAX;
-        }
+    public void setMax(Double max) {
+        percent.setMax(max);
     }
     
-    private void logMinMax() {
-        if (SHOW_LOG) {
-            LOG.log(Level.INFO, "***** address={0} | min={1} | max={2}", new Object[] {getAddress(), min, max});
-        }
-    }
-    
-    public int getMin() {
-        return min;
-    }
-
-    public void setMin(int min) {
-        this.min = min;
-    }
-
-    public int getMax() {
-        return max;
-    }
-
-    public void setMax(int max) {
-        this.max = max;
-    }
-
-    public boolean isCheckRange() {
-        return checkRange;
-    }
-
-    public void setCheckRange(boolean checkRange) {
-        this.checkRange = checkRange;
-    }
-
-    public boolean isAuto() {
-        return auto;
-    }
-
     public void setAuto(boolean auto) {
-        this.auto = auto;
+        percent.setAuto(auto);
     }
     
 }
